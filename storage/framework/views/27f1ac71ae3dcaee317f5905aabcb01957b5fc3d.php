1b24cac7970c6c352088c97d34046fe3645973b5
Nisha Baghel <dev5.apolloinfotech@gmail.com>
2026-04-01 10:42:05 +0530

    points master create

---
+++ storage/framework/views/27f1ac71ae3dcaee317f5905aabcb01957b5fc3d.php
@@ -0,0 +1,38 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+   
+    <meta name="author" content="">
+    <link rel="icon" href="<?php echo e(asset('assets/images/Newlogo.jpg')); ?>">
+
+    <!-- Title -->
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+    <?php echo $__env->yieldContent('opTag'); ?>
+
+    <!-- Necessary CSS Files -->
+    <link href="<?php echo e(asset ('front/css/bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo e(asset ('front/css/google-font.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset ('front/css/font-awesome.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset ('front/css/swiper.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset ('front/css/jquery.mb.YTPlayer.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset ('front/css/style.css')); ?>">
+
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" />
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   
+    <!-- FancyBox Image Gallery  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css" integrity="sha512-nNlU0WK2QfKsuEmdcTwkeh+lhGs6uyOxuUs+n+0oXSYDok5qy0EI0lt01ZynHq6+p/tbgpZ7P+yUb+r71wqdXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- FancyBox Image Gallery  -->
+
+
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=665d9f2b3a56e900196c1646&product=inline-share-buttons' async='async'></script>
+
+</head><?php /**PATH /var/www/html/evolve_business_live/resources/views/common/front/fronthead.blade.php ENDPATH**/ ?>
