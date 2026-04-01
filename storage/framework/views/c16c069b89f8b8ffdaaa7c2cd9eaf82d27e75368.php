ee179e4d229ce3f0e13887b5c09f7488a438dc07
Kush Shelat <dev4.apolloinfotech@gmail.com>
2026-04-01 19:02:50 +0530

    member and admin side changes

---
+++ storage/framework/views/c16c069b89f8b8ffdaaa7c2cd9eaf82d27e75368.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php echo $__env->make('auth.includes.head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<body>
+    <div class="auth-page-wrapper" style="overflow: hidden;">
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+    <?php echo $__env->make('auth.includes.scripts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</body>
+
+</html>
+<?php /**PATH C:\xampp laravel11\htdocs\live_codes\Evolve Business\resources\views/auth/layouts/app.blade.php ENDPATH**/ ?>
